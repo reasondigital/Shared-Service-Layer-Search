889a4cbbad672899bae26f8aaa7353fd89f89c12
Eji Osigwe <eji.osigwe@reasondigital.com>
2021-04-15 15:44:01 +0100

    Adjustments to the application base controller to include specific field errors where appropriate

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Response\ApiResponseBuilder;
 use App\Http\Response\JsonApiResponseBuilder;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Validator;
 
+/**
+ * Application base controller.
+ *
+ * @package App\Http\Controllers
+ * @since 1.0.0
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
@@ -16,23 +25,36 @@ class Controller extends BaseController
     const VALIDATION_ERROR_CODE = 'validation_error';
 
     /**
-     * @param \Illuminate\Http\Request $request
-     * @param array $rules
+     * @param  Request  $request
+     * @param  array  $rules
      *
-     * @return \App\Http\Response\JsonApiResponseBuilder
+     * @return JsonApiResponseBuilder
+     * @throws BindingResolutionException
+     * @since 1.0.0
      */
-    protected function validateRequest(Request $request, array $rules): JsonApiResponseBuilder {
-        $builder = new JsonApiResponseBuilder();
+    protected function validateRequest(Request $request, array $rules): JsonApiResponseBuilder
+    {
+        // Get response builder
+        $builder = app()->make(ApiResponseBuilder::class);
 
-        try {
-            $this->validate($request, $rules);
-        } catch (\Exception $e) {
-            // @todo - Does this need to loop over validation errors?
+        /*
+         * Get the validator instead of using `$this->validate()`.
+         * This is so we can get the individual field errors and add them to
+         * the response meta data alongside the response's main error message.
+         */
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
             $builder->setError(
                 400,
                 self::VALIDATION_ERROR_CODE,
-                $e->getMessage()
+                'The data provided was invalid. The request has not been fulfilled.'
             );
+
+            $fieldErrors = [];
+            foreach ($validator->errors()->toArray() as $field => $messages) {
+                $fieldErrors[$field] = $messages[0];
+            }
+            $builder->addMeta('field_errors', $fieldErrors);
         }
 
         return $builder;
