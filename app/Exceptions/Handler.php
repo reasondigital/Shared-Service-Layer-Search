ccc95f3f185b820c55dab831b38ef8725fa39ebc
Eji Osigwe <eji.osigwe@reasondigital.com>
2021-04-23 18:47:46 +0100

    Application now responds to "unauthenticated" errors with JSON

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use App\Http\Response\ApiResponseBuilder;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +42,22 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Force unauthenticated errors to respond as JSON.
+     *
+     * @param  Request                  $request
+     * @param  AuthenticationException  $exception
+     *
+     * @return JsonResponse
+     * @throws BindingResolutionException
+     * @since 1.0.0
+     */
+    protected function unauthenticated($request, AuthenticationException $exception): JsonResponse
+    {
+        $builder = app()->make(ApiResponseBuilder::class);
+        $builder->setError(401, 'unauthorised', $exception->getMessage());
+        return response()->json($builder->getResponseData(), $builder->getStatusCode());
     }
 }
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,19 +2,40 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
+     * Overrides the default to send a message more appropriate for this API.
+     *
+     * @param  Request  $request
+     * @param  array    $guards
+     *
+     * @throws AuthenticationException
+     * @since 1.0.0
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        throw new AuthenticationException(
+            'You are not authorised to consume this API',
+            $guards,
+            $this->redirectTo($request)
+        );
+    }
+
+    /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
+     *
      * @return string|null
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if (!$request->expectsJson()) {
             return route('login');
         }
     }
