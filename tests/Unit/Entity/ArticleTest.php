4d5095fa05c8b6c4b3afbcba175803cbf2b88a8d
Eji Osigwe <eji.osigwe@reasondigital.com>
2021-04-23 10:31:00 +0100

    Fixed tests after recent changes

--- tests/Unit/Entity/ArticleTest.php
+++ tests/Unit/Entity/ArticleTest.php
@@ -11,8 +11,7 @@ class ArticleTest extends TestCase
 {
     /**
      * Tests:
-     * - toSearchableArray converts the datePublished - Done
-     * - toArray will add schema data - Done
+     * - toResponseArray converts the datePublished - Done
      */
 
     /**
@@ -20,31 +19,18 @@ class ArticleTest extends TestCase
      */
     public function date_published_is_converted_if_datetime_object()
     {
-        $article = Article::factory()->make();
-
-        // Null if not a datetime
-        $date = date(DataConstants::API_ARTICLE_DATE_PUBLISHED_FORMAT);
-        $article->datePublished = $date;
-        $this->assertNull($article->toResponseArray()['datePublished']);
+        // Partial datetime should be cast to full
+        $article = Article::factory()->make([
+            'datePublished' => '2020-02-20',
+        ]);
+        $this->assertSame("2020-02-20 00:00:00", $article->toResponseArray()['datePublished']);
 
         // If a datetime object its converted to the specific format
         $now = new DateTime('now');
         $article->datePublished = $now;
         $this->assertSame(
-            $now->format(DataConstants::ELASTIC_DATETIME_FORMAT),
+            $now->format(DataConstants::API_ARTICLE_DATE_PUBLISHED_FORMAT),
             $article->toResponseArray()['datePublished']
         );
-    }
-
-    /**
-     * @test
-     */
-    public function to_array_add_schema_data()
-    {
-        $article = Article::factory()->make();
-
-        $data = $article->toArray();
-        $this->assertSame('https://schema.org', $data['@context']);
-        $this->assertSame('Article', $data['@type']);
     }
 }
