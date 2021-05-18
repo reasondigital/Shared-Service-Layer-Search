aa012e9a43667847f4448671af9d24df261c7fd9
Eji Osigwe <eji.osigwe@reasondigital.com>
2021-05-18 16:39:35 +0100

    Customised JSON response handler method for exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,8 @@ use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use ReflectionClass;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -59,5 +61,45 @@ class Handler extends ExceptionHandler
         $builder = app()->make(ApiResponseBuilder::class);
         $builder->setError(401, 'unauthorised', $exception->getMessage());
         return response()->json($builder->getResponseData(), $builder->getStatusCode());
+    }
+
+    /**
+     * Format the default JSON response for exceptions thrown in this
+     * application.
+     *
+     * @param  Request    $request
+     * @param  Throwable  $e
+     *
+     * @return JsonResponse
+     * @throws BindingResolutionException
+     * @since 1.0.0
+     */
+    protected function prepareJsonResponse($request, Throwable $e): JsonResponse
+    {
+        if ($this->isHttpException($e)) {
+            $code = $e->getStatusCode();
+            $errorCode = Str::snake((new ReflectionClass($e))->getShortName());
+            $errorCode = str_replace('_http_exception', '', $errorCode);
+            $headers = $e->getHeaders();
+        } else {
+            $code = 500;
+            $errorCode = 'internal_error';
+            $headers = [];
+        }
+
+        $builder = app()->make(ApiResponseBuilder::class);
+        $builder->setError($code, $errorCode, $e->getMessage());
+
+        if (config('app.debug') === true) {
+            $builder->addMeta('exception', $this->convertExceptionToArray($e));
+        }
+
+        return response()->json(
+            $builder->getResponseData(),
+            $builder->getStatusCode(),
+            $headers,
+            JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
+
     }
 }
